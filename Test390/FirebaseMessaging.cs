66d1980b4a0ba035eddee3c19e0640f9313a9e61
Ravi Gajadin <ravi.gajadin@nalta.com>
2020-10-16 14:15:29 +0200

    Add project files.

---
+++ Test390/FirebaseMessaging.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+using Firebase.Messaging;
+
+namespace Test390
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirebaseMessaging : FirebaseMessagingService
+    {
+        private const string Tag = "Test";
+
+        public override void OnNewToken(string p0)
+        {
+            base.OnNewToken(p0);
+        }
+
+        public override async void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(Tag, "Notification received from: " + message.From);
+        }
+    }
+}
